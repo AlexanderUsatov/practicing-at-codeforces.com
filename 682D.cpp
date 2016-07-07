4d06ecab7c29d43b4ac045793ab8dc0629ddb3ad
Mikhail Alpinskiy <ama1185@yandex.ru>
2016-07-08 00:33:18 +0300

    Update 682D.cpp

--- 682D.cpp
+++ 682D.cpp
@@ -43,7 +43,6 @@
 
 #include <vector>
 #include <string>
-#include <numeric>
 #include <algorithm>
 #include <iostream>
 
@@ -51,40 +50,41 @@ using namespace std;
 
 int main()
 {
-    size_t unused, k;
-    cin >> unused >> unused >> k;
+    size_t unused, cnt = 4;
+    cin >> unused >> unused >> cnt;
     string empty, s, t;
     getline(cin, empty);
     getline(cin, s);
     getline(cin, t);
 
     vector<vector<vector<vector<size_t>>>> m(
-        s.length(), vector<vector<vector<size_t>>>(
-        t.length(), vector<vector<size_t>>(
-        k, vector<size_t>(2, 0))));
-    for (size_t i = 1; i < s.length(); i++)
-    {
-        for (size_t j = 1; j < t.length(); j++)
-        {
-            for (size_t l = 1; l < k; l++)
-            {
-                //m[i][j][l][0] = ;
+        s.length() + 1, vector<vector<vector<size_t>>>(
+        t.length() + 1, vector<vector<size_t>>(
+        cnt + 1, vector<size_t>(
+        2, 0))));
+    for (size_t i = 0; i < s.length(); i++) {
+        for (size_t j = 0; j < t.length(); j++) {
+            for (size_t k = 0; k < cnt; k++) {
+                for (size_t e = 0; e < 2; e++) {
+                    m[i + 1][j][k][0] = max(m[i + 1][j][k][0], m[i][j][k][e]);
+                    m[i][j + 1][k][0] = max(m[i][j + 1][k][0], m[i][j][k][e]);
+                }
+
+                if (s[i] == t[j]) {
+                    m[i + 1][j + 1][k][1] = max(m[i + 1][j + 1][k][1], m[i][j][k][1] + 1);
+                    m[i + 1][j + 1][k + 1][1] = max(m[i + 1][j + 1][k + 1][1], m[i][j][k][0] + 1);
+                }
             }
         }
     }
 
     size_t valueMax = 0;
-    for (size_t i = 1; i < s.length(); i++)
-    {
-        for (size_t j = 1; j < t.length(); j++)
-        {
-            for (size_t l = 1; l < k; l++)
-            {
-                for (size_t n = 0; n < 2; n++)
-                {
-                    if (valueMax = m[i][j][l][n])
-                    {
-                        valueMax = m[i][j][l][n];
+    for (size_t i = 1; i < s.length() + 1; i++) {
+        for (size_t j = 1; j < t.length() + 1; j++) {
+            for (size_t k = 0; k < cnt; k++) {
+                for (size_t e = 0; e < 2; e++) {
+                    if (valueMax < m[i][j][k][e]) {
+                        valueMax = m[i][j][k][e];
                     }
                 }
             }
