854938920eca4a5adaf5a70d28734e1a4aae0dd7
Mikhail Alpinskiy <ama1185@yandex.ru>
2016-07-19 15:52:36 +0300

    Update 689E.cpp

--- 689E.cpp
+++ 689E.cpp
@@ -1,72 +1,43 @@
 //
-// http://codeforces.com/problemset/problem/689/E
+// http://codeforces.com/contest/689/problem/E
 //
-#include <map>
 #include <iostream>
-#include <string>
+#include <vector>
+#include <deque>
+#include <algorithm>
+#include <iterator>
+#include <fstream>
 
 using namespace std;
-int const mod = 1000000007;
-const int nmax = 2e5 + 5;
-
-int f[nmax]; // factorial(i)
-int c[nmax]; // 1 / factorial(i)
-
-int chooseMod(int n, int k)
-{
-    int ans = (1ll * f[n] * c[k]) % mod;
-    return (1ll * ans * c[n - k]) % mod;
-}
-
-int powMod(int a,int b)
-{
-    int ans = 1;
-    while (b)
-    {
-        if (b&1) ans = (1ll * ans * a) % mod;
-        a = (1ll * a * a) % mod;
-        b /= 2;
-    }
-
-    return ans;
-}
 
 int main()
 {
-    int n, k;
-    cin >> n >> k;
-    string empty;
-    getline(cin, empty);
+	auto const input = vector<int>(istream_iterator<int>(cin), istream_iterator<int>());
+	auto const n = input[0];
+	auto const a = vector<int>(input.begin() + 1, input.begin() + 1 + n);
+	auto const b = vector<int>(input.begin() + 1 + n, input.end());
 
-    map<int,int> events;
-    for (int i = 0; i < n; i++) {
-        int l, r;
-        cin >> l >> r;
-        getline(cin, empty);
-        ++events[l];
-        --events[r + 1];
-    }
+	int ans = 0;
+	deque<int> ad, bd;
+	for (int i = 0, j = 0; i < n; i++)
+	{
+		for (; !ad.empty() && ad.back() <= a[i]; ad.pop_back());
+		for (; !bd.empty() && bd.back() <= b[i]; bd.pop_back());
+		ad.push_back(i);
+		bd.push_back(i);
 
-    f[0] = c[0] = 1;
-    for (int i = 1;i <= n;++i)
-    {
-        f[i] = (1ll * f[i-1] * i) % mod;
-        c[i] = powMod(f[i], mod-2);
-    }
+		for (; j <= i && !ad.empty() && !bd.empty() && a[ad.front()] > b[bd.front()];)
+		{
+			j++;
+			for (; !ad.empty() && ad.front() < j; ad.pop_front());
+			for (; !bd.empty() && bd.front() < j; bd.pop_front());
+		}
 
-    int balance = 0,
-       prev = events.begin()->first,
-       sum = 0;
-    for (auto it: events)
-    {
-        if (balance >= k) {
-            sum += (1LL * (it.first - prev) * chooseMod(balance, k)) % mod;
-            sum %= mod;
-        }
+		if (!ad.empty() && !bd.empty() && a[ad.front()] == b[bd.front()])
+		{
+			ans += min(ad.front(), bd.front()) - j + 2;
+		}
+	}
 
-        balance += it.second;
-        prev = it.first;
-    }
-
-    cout << sum << endl;
+	cout << ans << endl;
 }
