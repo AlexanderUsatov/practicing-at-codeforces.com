368a910a00d4883b1b9c81fa8c03f820394245b4
Mikhail Alpinskiy <ama1185@yandex.ru>
2016-07-22 22:03:00 +0300

    Create 384B.cpp

---
+++ 384B.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, m, k;
+    cin >> n >> m >> k;
+    cout << (m * (m - 1)) / 2 << endl;
+    if (k == 0)
+    {
+        for (int i = 1; i < m; i++)
+            for (int j = i + 1; j <= m; j++)
+                cout << i << ' ' << j << endl;
+    }
+    else
+    {
+        for (int i = m; i > 1; i--)
+            for (int j = i - 1; j > 0; j--)
+                cout << i << ' ' << j << endl;
+    }
+}
