ea3e5450eedf74422bc7b17a4f6ad706048c5813
Mikhail Alpinskiy <ama1185@yandex.ru>
2016-09-15 13:01:10 +0300

    Update 455A.cpp

--- src/455A.cpp
+++ src/455A.cpp
@@ -1,28 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int const N = 100000;
-int n, a[N], d[N][3] = {};
+int const N = 100001;
+int n, a[N] = {}, m = 0, d[N][3] = {};
 
 int main()
 {
   //ifstream input("input.txt"); cin.rdbuf(input.rdbuf());
   cin >> n;
   for (auto i = 0; i < n; i++)
-    cin >> a[i];
-
-  for (auto j = 0; j < n; j++)
   {
-    for (auto i = 0; i < n; i++)
+    int k; cin >> k;
+    a[k - 1]++;
+    m = max(m, k);
+  }
+
+  for (auto j = 0; j < m; j++)
+  {
+    for (auto i = 0; i < m; i++)
     {
-      auto const k = j%3;
-      auto const k_1 = (k + 2)%3;
-      auto const k_2 = (k + 1)%3;
+      auto const k = j % 3;
+      auto const k_1 = (k + 2) % 3;
+      auto const k_2 = (k + 1) % 3;
       d[i][k] = max(
         d[i][k_1],
-        d[i][k_2] + a[j]);
+        d[i][k_2] + (j + 1)*a[j]);
     }
   }
 
-  cout << d[n - 1][(n - 1)%3] << endl;
+  cout << d[m - 1][(m - 1) % 3] << endl;
 }
