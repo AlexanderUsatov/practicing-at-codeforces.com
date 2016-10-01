e6ad9bf1bf7f99640f7bef6b13ee2fbea464a2cf
Mikhail Alpinskiy <ama1185@yandex.ru>
2016-10-01 13:49:06 +0300

    Update 368B.cpp

--- src/368B.cpp
+++ src/368B.cpp
@@ -1,1 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int const N = 100002;
+int n, m, a[N], cnt[N] = {}, d[N] = {};
+
+int main()
+{
+    //fstream input("input.txt"); cin.rdbuf(input.rdbuf());
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+
+    for (int i = n; i > 0; i--) {
+        d[i] = d[i + 1];
+        if (cnt[a[i]]++ == 0)
+            d[i]++;
+    }
+
+    for (int i = 0; i < m; i++) {
+        int l; cin >> l;
+        cout << d[l] << '\n';
+    }
+}
