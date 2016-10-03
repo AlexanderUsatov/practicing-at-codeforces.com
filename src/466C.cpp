052fa135a3db4bd3ca0cbc7e0b7bfb7f8a178a33
Mikhail Alpinskiy <ama1185@yandex.ru>
2016-10-03 19:40:36 +0300

    Update 466C.cpp

--- src/466C.cpp
+++ src/466C.cpp
@@ -1,28 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int const N = 5*100000 + 1;
-int a[N], n;
-long long d[N];
+int const N = 5 * 100000 + 1;
+int n, a[N], cnt[N] = {};
 
 int main()
 {
     //fstream input("input.txt"); cin.rdbuf(input.rdbuf());
     cin >> n;
+    auto s3 = 0LL;
     for (int i = 1; i <= n; i++) {
         cin >> a[i];
-        d[i] = d[i - 1] + a[i];
+        s3 += a[i];
     }
 
-    int ret = 0;
+    if (s3 % 3 != 0) {
+        cout << 0 << endl;
+        return 0;
+    }
+
+    s3 /= 3;
+    auto s = 0LL;
+    for (int i = 1; i < n - 1; i++) {
+        cnt[i] = cnt[i - 1];
+        s += a[i];
+        if (s == s3)
+            cnt[i]++;
+    }
+
+    s = 0LL;
+    auto ret = 0LL;
     for (int i = n; i > 2; i--) {
-        for (int j = 2; j < i; j++) {
-            auto const sum1 = d[j - 1];
-            auto const sum2 = d[i - 1] - d[j - 1];
-            auto const sum3 = d[n] - d[i - 1];
-            if (sum1 == sum2 && sum2 == sum3)
-                ret++;
-        }
+        s += a[i];
+        if (s == s3)
+            ret += cnt[i - 2];
     }
 
     cout << ret << endl;
