788bba3c116dcd319e30094c7d6b7c55eb890dfe
Mikhail Alpinskiy <ama1185@yandex.ru>
2016-10-05 11:16:37 +0300

    Update 538B.cpp

--- src/538B.cpp
+++ src/538B.cpp
@@ -1,35 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int const aN = 100;
-int n, a[aN], d[aN];
+int n, a[10];
 
 int main()
 {
     //fstream input("input.txt"); cin.rdbuf(input.rdbuf());
     cin >> n;
 
-    for (int i = 1, k = 1; i < aN; k*=10) {
-        a[i++] = k;
-        for (int j = 1, u = i - 1; j < u; j++)
-            a[i++] = k + a[j];
-    }
-
     int ret = 0;
-    for (; n > 0;) {
-        auto it = lower_bound(a, a + aN, n);
-        if (*it == n) {
-            d[ret++] = *it;
-            break;
+    for (; n != 0;) {
+        int m = n, k = 0;
+        for (int p = 1; m != 0;) {
+            if (m % 10 != 0)
+                k += p;
+            p *= 10;
+            m /= 10;
         }
-        else {
-            n -= *(--it);
-            d[ret++] = *it;
-        }
+
+        a[ret++] = k;
+        n -= k;
     }
 
-    cout << ret << endl;
-    for (int i = 0; i < ret; i++)
-        cout << d[i] << ' ';
-    cout << endl;
+    cout << ret << '\n';
+    copy(a, a + ret, ostream_iterator<int>(cout, " "));
 }
