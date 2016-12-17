987a6c96ad2446c0d7a52cd13be87560c65a27c4
Mikhail Alpinskiy <ama1185@yandex.ru>
2016-12-18 00:18:05 +0300

    Create 702A.cpp

---
+++ src/702A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, a[100000];
+
+int main() {
+    ifstream input("input.txt");
+    cin.rdbuf(input.rdbuf());
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    int m = 0;
+    for (int i = 0; i < n;) {
+        int j = i + 1;
+        for (; i < n && j < n && a[j - 1] < a[j]; j++);
+        m = max(m, j - i);
+        i = j;
+    }
+
+    cout << m << endl;
+}
