5835b027f2fa1b4130df5e3315448408c69b573a
Mikhail Alpinskiy <ama1185@yandex.ru>
2016-12-19 13:44:37 +0300

    Create 550C.cpp

---
+++ src/550C.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printYES(int n) {
+    cout << "YES" << endl;
+    cout << n << endl;
+}
+
+int main() {
+    //ifstream input("input.txt");
+    //cin.rdbuf(input.rdbuf());
+
+    string s;
+    getline(cin, s);
+    int const n = s.length();
+
+    for (int i = 0; i < n; i++) {
+        int const si = s[i] - '0';
+        if (si % 8 == 0) {
+            printYES(si);
+            return 0;
+        }
+
+        for (int j = i + 1; j < n; j++) {
+            int const sj = s[j] - '0';
+            int const sij = si * 10 + sj;
+            if (sij % 8 == 0) {
+                printYES(sij);
+                return 0;
+            }
+
+            for (int k = j + 1; k < n; k++) {
+                int const sk = s[k] - '0';
+                int const sijk = sij * 10 + sk;
+                if (sijk % 8 == 0) {
+                    printYES(sijk);
+                    return 0;
+                }
+            }
+        }
+    }
+
+    cout << "NO" << endl;
+}
